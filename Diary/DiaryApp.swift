726da7a8cd18aa6f272e0651ec817e3ec5ee9772
yyokii <yyokii.h@gmail.com>
2023-04-23 17:31:42 +0900

    Initial Commit

---
+++ Diary/DiaryApp.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryApp.swift
+//  Diary
+//
+//  Created by Higashihara Yoki on 2023/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DiaryApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
