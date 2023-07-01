daca1df5d9f9318327c71983786205810457734e
yyokii <yyokii.h@gmail.com>
2023-07-01 15:57:39 +0900

    🩹

--- Diary/View/IconWithRoundedBackground.swift
+++ Diary/View/IconWithRoundedBackground.swift
@@ -14,6 +14,7 @@ struct IconWithRoundedBackground: View {
 
     var body: some View {
         Image(systemName: systemName)
+            .foregroundColor(.white)
             .frame(width: 30, height: 30)
             .bold()
             .background {
